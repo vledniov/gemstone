afa61f7db630959b3160f5afd0506936bed4ded7
vledniov <vledniov@nebulent.com>
2014-04-17 15:18:42 +0300

    Add getters and setters for Globals class

--- railsFTW/railsFTW/Globals.cs
+++ railsFTW/railsFTW/Globals.cs
@@ -7,10 +7,28 @@ namespace railsFTW
 {
     class Globals
     {
-        public static String PROJECT_DIR = "";
+        private static String  PROJECT_DIR = "";
+        private static Boolean RSPEC;
+        private static Boolean CAPYBARA;
 
-        public static void setDirectory(String dir){
+        public static void setDirectory(String dir)
+        {
             PROJECT_DIR = dir;
+        }
+
+        public static String getDirectory()
+        {
+            return PROJECT_DIR;
+        }
+
+        public static void setRspec(Boolean val)
+        {
+            RSPEC = val;
+        }
+
+        public static void setCapybara(Boolean val)
+        {
+            CAPYBARA = val;
         }
     }
 }
